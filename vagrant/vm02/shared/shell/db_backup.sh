7f00c882284524da7c32c0fc44fecef7c1860f54
minamihiroto <kokowatamf@gmail.com>
2020-09-01 17:28:20 +0900

    ✨Add php-fpm,db-shell,nginx Configuration

---
+++ vagrant/vm02/shared/shell/db_backup.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+db='menta_db'
+user='root'
+pass='hiroto373'
+backupdir='/home/menta/db_backup'
+num=7
+
+filename=${db}_`date +%y%m%d_%H%M`.sql
+MYSQL_PWD=${pass} mysqldump -u${user} --set-gtid-purged=OFF ${db} > ${backupdir}/${filename}
+chmod 600 ${backupdir}/${filename}
+find ${backupdir} -name "${db}_*.sql" -mtime +${num} -delete

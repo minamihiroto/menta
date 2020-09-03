5c430c8f65b53353d1f215011a1460c69a416d1e
minamihiroto <kokowatamf@gmail.com>
2020-09-03 12:02:05 +0900

    🔖Update db_backup.sh

--- vagrant/vm02/shared/shell/db_backup.sh
+++ vagrant/vm02/shared/shell/db_backup.sh
@@ -4,9 +4,9 @@ db='menta_db'
 user='root'
 pass='hiroto373'
 backupdir='/var/www/db_backup'
-num=7
+oldfile=`date --date "5 days ago" +%y%m%d_%H%M`
 
 filename=${db}_`date +%y%m%d_%H%M`.sql
 MYSQL_PWD=${pass} mysqldump -u${user} --set-gtid-purged=OFF ${db} > ${backupdir}/${filename}
 chmod 600 ${backupdir}/${filename}
-find ${backupdir} -name "${db}_*.sql" -mtime +${num} -delete
+rm ${backupdir}/"${db}_${oldfile}.sql"

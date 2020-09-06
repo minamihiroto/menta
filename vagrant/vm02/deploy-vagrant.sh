5d5929d7939fdb8b893f39d3a8cdde26d8ba3a5e
minamihiroto <kokowatamf@gmail.com>
2020-09-06 21:36:50 +0900

    🔖Update deploy-vagrant.sh add message

--- vagrant/vm02/deploy-vagrant.sh
+++ vagrant/vm02/deploy-vagrant.sh
@@ -3,15 +3,19 @@ cd `dirname $0`
 PROCESS_NAME=vagrant
 
 if [ $# = 0 ]; then
-  echo "下記のようにstatusを指定して実行してください。(例) ./deploy-vagrant.sh  status,up,suspend"
+  echo "下記のように引数を指定して実行してください。(例) ./deploy-vagrant.sh  status,up,suspend"
  else
-   if [ $1 = 'status' ]; then
-     $PROCESS_NAME status
-   fi
-   if [ $1 = 'up' ]; then
-     $PROCESS_NAME up
-   fi
-   if [ $1 = 'suspend' ]; then
-     $PROCESS_NAME suspend
-   fi
+  if [ $1 = 'status' ]; then
+    $PROCESS_NAME status
+  else
+    if [ $1 = 'up' ]; then
+      $PROCESS_NAME up
+    else
+      if [ $1 = 'suspend' ]; then
+        $PROCESS_NAME suspend
+      else
+        echo "正しい引数を指定してください。(例) ./deploy-vagrant.sh  status,up,suspend"
+      fi
+    fi
+  fi
 fi

64743c381d194e88ec54fa70b60e49c3fd9ee8f7
minamihiroto <kokowatamf@gmail.com>
2020-09-06 16:11:01 +0900

    🔖Update deploy-vagrant.sh

--- vagrant/vm02/deploy-vagrant.sh
+++ vagrant/vm02/deploy-vagrant.sh
@@ -1,16 +1,17 @@
 #! /bin/bash
 cd `dirname $0`
+PROCESS_NAME=vagrant
 
 if [ $# = 0 ]; then
   echo "下記のようにstatusを指定して実行してください。(例) ./deploy-vagrant.sh  status,up,suspend"
  else
-   if [ $# = 'status' ]; then
-     vagrant status
+   if [ $1 = 'status' ]; then
+     $PROCESS_NAME status
    fi
-   if [ $# = 'up' ]; then
-     vagrant up
+   if [ $1 = 'up' ]; then
+     $PROCESS_NAME up
    fi
-   if [ $# = 'suspend' ]; then
-     vagrant suspend
+   if [ $1 = 'suspend' ]; then
+     $PROCESS_NAME suspend
    fi
 fi

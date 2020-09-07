38c0defd4883233480ea3365aa6e17714c743079
minamihiroto <kokowatamf@gmail.com>
2020-09-07 22:52:01 +0900

    🔖Update deploy-vagrant.sh add parameter-variable

--- vagrant/vm02/deploy-vagrant.sh
+++ vagrant/vm02/deploy-vagrant.sh
@@ -1,21 +1,10 @@
 #! /bin/bash
 cd `dirname $0`
 PROCESS_NAME=vagrant
+PROCESS_PARAMETER=$1
 
 if [ $# = 0 ]; then
   echo "下記のように引数を指定して実行してください。(例) ./deploy-vagrant.sh  status,up,suspend"
- else
-  if [ $1 = 'status' ]; then
-    $PROCESS_NAME status
-  else
-    if [ $1 = 'up' ]; then
-      $PROCESS_NAME up
-    else
-      if [ $1 = 'suspend' ]; then
-        $PROCESS_NAME suspend
-      else
-        echo "正しい引数を指定してください。(例) ./deploy-vagrant.sh  status,up,suspend"
-      fi
-    fi
-  fi
+else
+  $PROCESS_NAME $PROCESS_PARAMETER
 fi
